4b102a0adcc31e96efa46c80794f96632569bbbd
Agha Naveed <naveedabs31@gmail.com>
2024-05-26 22:44:00 +0500

    Animation to the Skills section

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ menuCloseBtn.addEventListener('click', () => {
     menu.classList.add('r-hide')
     menuCloseBtn.classList.add('r-hide')
 })
+
+window.addEventListener('scroll', checkBoxes)
+
+let skillsRow1 = document.querySelectorAll('.skills-row')[0]
+let skillsRow2 = document.querySelectorAll('.skills-row')[1]
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    const boxTop = skillsRow1.getBoundingClientRect().top;
+    if(boxTop < triggerBottom) {
+        skillsRow1.classList.add('l-show')
+        skillsRow2.classList.add('r-show')
+    }
+    else {
+        skillsRow1.classList.remove('l-show')
+        skillsRow2.classList.remove('r-show')
+    }
+}
