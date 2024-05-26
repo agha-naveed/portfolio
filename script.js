47473a4425337045a14fa7c0eab48fda844898f8
Agha Naveed <naveedabs31@gmail.com>
2024-05-26 22:07:33 +0500

    Skills Section Added

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let searchBtn = document.querySelector('.search-btn-click')
 let respSearch = document.querySelector('.resp-search')
-let searchCrossBtn = document.querySelector('.fa-xmark')
+let searchCloseBtn = document.querySelector('.search-close-btn')
 
 searchBtn.addEventListener('click', () => {
     respSearch.style.display = 'block'
 })
-searchCrossBtn.onclick = () => {
+searchCloseBtn.onclick = () => {
     respSearch.style.display = 'none'
 }
+
+let menuBtn = document.querySelector('.menu-bar')
+let menuCloseBtn = document.querySelector('.menu-bar > .menu-close-btn')
+let menu = document.querySelector('.navbar > nav > ul')
+
+menuBtn.addEventListener('click', () => {
+    menu.style.right = 0
+    menu.classList.remove('r-hide')
+    menuCloseBtn.classList.remove('r-hide')
+})
+
+menuCloseBtn.addEventListener('click', () => {
+    menu.classList.add('r-hide')
+    menuCloseBtn.classList.add('r-hide')
+})
