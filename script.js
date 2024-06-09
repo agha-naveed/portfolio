cfb63070d816a7d7207c400def35348d562506a2
Agha Naveed <naveedabs31@gmail.com>
2024-06-09 11:27:54 +0500

    Custom Cursor

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+// Custom Cursor
+let cursor = document.getElementById('cursor')
+// let cursorBorder = document.getElementById('cursor-border')
+let cursorBorder = document.querySelector('.cursor-border')
+
+window.onmousemove = (e) => {
+    
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+
+    if(!cursorBorder.classList.contains('cursor-large')) {
+        cursorBorder.style.left = -7.3 + e.clientX + 'px';
+        cursorBorder.style.top = -7.5 + e.clientY + 'px';
+    }
+    else {
+        cursorBorder.style.left = -25 + e.clientX + 'px';
+        cursorBorder.style.top = -28 + e.clientY + 'px';
+    }
+}
+
+document.addEventListener('mousemove', function(e) {
+    let eventChecker = e.target
+    if (eventChecker.closest('.navbar ul') || eventChecker.closest('.btn-shine a') || eventChecker.closest('#search-input') || eventChecker.closest('.contact-me-btn a') || eventChecker.closest('.skills-box')) {
+        cursorBorder.classList.add('cursor-large')
+    } else {
+        cursorBorder.classList.remove('cursor-large')
+    }
+  });
+
+
+
 let searchBtn = document.querySelector('.resp-search-box')
 let respSearch = document.querySelector('.resp-search')
 let searchCloseBtn = document.querySelector('.search-close-btn')
-
 
 searchBtn.addEventListener('click', () => {
     respSearch.classList.add('search-cover-show')
@@ -74,6 +104,7 @@ function checkBoxes() {
         skillsRow2.classList.remove('r-show')
     }
 }
+
 
 checkBoxes()
 window.addEventListener('scroll', checkBoxes)
