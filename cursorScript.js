097be48f6ce959604c35cb0f9a6781c6dbde9606
Agha Naveed <naveedabs31@gmail.com>
2024-06-10 16:28:52 +0500

    New Script For Cursor

---
+++ cursorScript.js
@@ -0,0 +1,29 @@
+// Custom Cursor
+let cursor = document.getElementById('cursor')
+// let cursorBorder = document.getElementById('cursor-border')
+let cursorBorder = document.querySelector('.cursor-border')
+
+window.onmousemove = (e) => {
+    
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+
+    if(!cursorBorder.classList.contains('cursor-large')) {
+        cursorBorder.style.left = -7.3 + e.clientX + 'px';
+        cursorBorder.style.top = -7.5 + e.clientY + 'px';
+    }
+    else {
+        cursorBorder.style.left = -25 + e.clientX + 'px';
+        cursorBorder.style.top = -28 + e.clientY + 'px';
+    }
+}
+
+document.addEventListener('mousemove', function(e) {
+    let eventChecker = e.target
+    if (eventChecker.closest('.navbar ul') || eventChecker.closest('.btn-shine a') || eventChecker.closest('#search-input') || eventChecker.closest('.contact-me-btn a') || eventChecker.closest('.skills-box')) {
+        cursorBorder.classList.add('cursor-large')
+    } else {
+        cursorBorder.classList.remove('cursor-large')
+    }
+  });
+
